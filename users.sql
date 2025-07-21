9c2fde570ae7271fb9729bba4d81083f13fbb439
Harry Savill <160655325+harrySavill@users.noreply.github.com>
2025-07-21 15:15:57 +0100

    Delete users.sql

--- users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
-    `user_id` INT(11) NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(100) NOT NULL UNIQUE,
-    `email` VARCHAR(100) NOT NULL UNIQUE,
-    `password` VARCHAR(255) NOT NULL, 
-    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	`account_type` ENUM('admin', 'user') DEFAULT 'user',
-    PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
